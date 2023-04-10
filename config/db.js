def5ad52a1be61ae149f40f7c0f6acd689b2c100
FelpsDevJs <sr.felipe.viana@gmail.com>
2023-04-09 23:32:00 -0300

    update mongo connect

--- config/db.js
+++ config/db.js
@@ -1,18 +1,24 @@
-const  mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
+// Função para conectar-se ao MongoDB
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.DB_URL,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        })
-        console.log(`MONGODB CONNECTED: ${conn.connection.host} 🏦`)
+  try {
+    // Conecte-se ao banco de dados usando a URL fornecida em variável de ambiente
+    const conn = await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
 
-    } catch (error) {
-        console.error(`ERROR: ${error.message}`)
-        process.exit(1);
+    // Exiba uma mensagem de confirmação de que a conexão foi bem-sucedida
+    console.log(`MONGODB CONNECTED: ${conn.connection.host} 🏦`);
+  } catch (error) {
+    // Exiba uma mensagem de erro se não for possível conectar ao MongoDB
+    console.error(`ERROR: Unable to connect to MongoDB: ${error.message}`);
 
-    }
-}
+    // Saia do processo com código de status 1 para indicar um erro
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+// Exporte a função connectDB para ser usada em outros arquivos
+module.exports = connectDB;
